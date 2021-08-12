2183512e6ca8d13129b5ea883ea33415206d6400
Horse <42266290+Horsyy@users.noreply.github.com>
2021-08-12 18:02:55 +0300

    Create rdx_animalharvest.sql

---
+++ rdx_animalharvest.sql
@@ -0,0 +1,20 @@
+USE `redm_extended`;
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('GameyMeat', 'Big Game Meat', 1, 0, 1),
+	('item_birdegg', 'Bird Egg', 1, 0, 1),
+	('item_bull_horn', 'Ram Horn', 1, 0, 1),
+	('item_catfish', 'Catfish', 1, 0, 1),
+	('item_feather', 'Feather', 1, 0, 1),
+	('item_lizzardskin', 'Lizzard Skin', 1, 0, 1),
+	('item_muskie', 'Muskie', 1, 0, 1),
+	('item_northpike', 'Northern Pike', 1, 0, 1),
+	('item_possumpelt', 'Possum Pelt', 1, 0, 1),
+	('item_rabbitskin', 'Rabbit Skin', 1, 0, 1),
+	('item_racoonskin', 'Racoon Skin', 1, 0, 1),
+	('item_skunkskin', 'Skunk Skin', 1, 0, 1),
+	('item_snakeskin', 'Snake Skin', 1, 0, 1),
+	('item_sturgeon', 'Sturgeon', 1, 0, 1),
+	('item_turtle_shell', 'Turtle Shell', 1, 0, 1)
+	('Meat', 'Meat', 1, 0, 1)
+;
